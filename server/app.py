b93aafabdd97502bf0cf84297c89f279abcaf4e0
Andrew Katsumi Sloan <andrewkatsumisloan@gmail.com>
2023-07-29 11:35:36 -0700

    Add basic fibonacci calculation route.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Just some boilerplate!'
+    return 'Test!'
+
+@app.route('/generate_fib', methods=['POST'])
+def generate_fib():
+    data = request.get_json()
+    n = data.get('NFib', 5)
+
+    fibonacci_numbers = [0, 1]
+    for i in range(2, n):
+        next_fib = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_fib)
+
+    return jsonify({'fibonnaciNumbers': fibonacci_numbers})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
